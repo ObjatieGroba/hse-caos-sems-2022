92d868ed104ba68dccee8d9e7b9a013d6018f293
Igor Mineev <igron99@mail.ru>
2023-01-20 12:33:27 +0300

    Fix sem 06

--- sm06/a.c
+++ sm06/a.c
@@ -13,4 +13,14 @@ int main() {
     asm("mov %[x], %[z] \n sub %[y2], %[z]" :
         [z]"=rm"(z) : [x]"rm"(x), [y2]"rm"(y));
     printf("%d\n", z);
+
+    asm("mov $2, %%eax" : "=a"(z));  /// z value store in eax
+
+    z = 1;
+    int vvv;
+    asm("mov %%edx, %%eax\n"
+        "mov $2, %%edx\n" : "+d"(z), "=a"(vvv));
+
+    /// vvv == old_z; z == 2
+
 }
